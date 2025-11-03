98baf787692f022030e61b2e75072ebccb92c9df
VIVEK BHARDWAJ <bvivek514@gmail.com>
2025-11-03 18:11:28 +0000

    try new

---
+++ 01_viteReact/src/try.jsx
@@ -0,0 +1,6 @@
+function Viv(){
+    return(
+        <h2>Vivek buddy</h2>
+    )
+}
+export default Viv;
---
+++ 01basicreact/src/Vive.js
@@ -0,0 +1,6 @@
+function Vivek() {
+    return (
+        <h2>Vivek buddy</h2>
+    )
+}
+export default Vivek;
